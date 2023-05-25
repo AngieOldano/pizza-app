4d2a6bb423376ce03eff0d37089179a1ccb20417
root <angiedenise.oldano@estudiantes.unahur.edu.ar>
2023-05-25 10:57:34 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Header from './components/Header';
+
+
+function App() {
+
+
+  return (
+    <div className='p-3 mb-2 bg-primary text-white'>
+      <Header/>
+      <h1>HOla</h1>
+    </div>
+  );
+}
+
+export default App;
