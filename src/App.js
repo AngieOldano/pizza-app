5b26729c5b357b02dfc647ddf1a6eb1e371d1f44
root <angiedenise.oldano@estudiantes.unahur.edu.ar>
2023-05-28 13:30:32 -0300

    arreglado zoom pizzas

--- src/App.js
+++ src/App.js
@@ -18,28 +18,29 @@ function App() {
     <div className=''>
       <Header/>
       
-      <div className="background-image row">
-        <div className='col-7'>
+      <div className="mt-5 background-image row">
 
+        <div className='col-7'>
         </div>
+
         <div className='col-5 text-white align-self-center'>
           <div className='mt-5'>
             <h1 className='display-1'>We Make the Best Pizza in Town</h1>
-            <h3>Our number one mission is to deliver the best possible</h3>
-            <h3>pizza for the best possible price for our customers. </h3>
+            <h3>Our number one mission is to deliver the best possible pizza for the best possible price for our customers. </h3>
           </div>
-        </div>   
+        </div> 
+
       </div>
 
-      <div className='mt-5 mb-5 row container-fluid'>
-        <div className='col-6 d-flex justify-content-end '>
+      <div className='mt-5 mb-5 row container-fluid column-gap-5'>
+        <div className='col-7 d-flex justify-content-center pr-1'>
           <article className='align-self-center'>
             <h1>Your Favorite Pizza,on the Way!</h1>
             <h3>Always on time</h3>
-            <p>We care so that our orders arrive as soon as possible in the best conditions</p>
+            <h5>We care so that our orders arrive as soon as possible in the best conditions</h5>
           </article>
         </div>
-        <div className='col-6 d-flex justify-content-center'>
+        <div className='col-4 d-flex pr-5 justify-content-center'>
           <img className='rounded-4' width="600" height="400" src={pizzaimage} alt="description of image" />
         </div>
       </div>
@@ -58,97 +59,119 @@ function App() {
         </div>
       </div>
 
+
       <div className='mb-5'>
         <div className='d-flex justify-content-center'>
           <article className='mt-5'>
             <h1>OUR PIZZAS</h1>
           </article>
         </div>
-
         
         <div className=''>
 
-          <div className='mb-3 row d-flex justify-content-center'>
+          <div className='mb-3 row '>          
+            <div className='pizzas-container d-flex justify-content-center grid gap-0 column-gap-3'> 
+              <div className='col-4 pizza-image-container'>
+                <img className='rounded-4 zoom-pizza' width="400" height="400" src={onion} alt="description of image"/>
+                <div class="overlay">
+                  <div class="text-overlay">
+                    <p>ONION SPECIAL</p> <p>Sauce, mozzarella, onion cut into feathers, cherry tomatoes confit, black olives</p>
+                  </div>
+                </div>
+              </div>
+            
+              
+          
+              <div className='col-4 pizza-image-container'>
+                <img className='rounded-4 zoom-pizza' width="400" height="400" src={veggie} alt="description of image" />
+                <div class="overlay">
+                  <div class="text-overlay">
+                    <p>VEGGIE</p> <p>Sauce, muzza, zucchini, roasted, roasted eggplant, candied cherry tomatoes, black olives, and basil leaves</p>
+                  </div>
+                </div>
+              </div>
+         
+              <div className='col-4 pizza-image-container'>
+                <img className='rounded-4 zoom-pizza' width="400" height="400" src={hell} alt="description of image"/>
+                <div class="overlay">
+                  <div class="text-overlay">
+                    <p>HELL</p> <p>Sauce, mozza, sliced bacon, sliced green jalapeños, red jalapeño sauce, red onion, shredded provolone and basil leaves</p>
+                  </div>
+                </div>
+              </div>  
 
-            <div className='col-4 images-pizzas-container'>
-              <img className='rounded-4 images-pizza' width="400" height="400" src={onion} alt="description of image"/>
-              <div class="overlay">
-                <div class="text-overlay">
-                  <p>ONION SPECIAL</p> <p>Sauce, mozzarella, onion cut into feathers, cherry tomatoes confit, black olives</p>
+            </div> 
+          </div>
+          <div className='mb-3 row '>          
+            <div className='pizzas-container d-flex justify-content-center grid gap-0 column-gap-3'> 
+              <div className='col-4 pizza-image-container'>
+                <img className='rounded-4 zoom-pizza' width="400" height="400" src={modern} alt="description of image"/>
+                <div class="overlay">
+                  <div class="text-overlay">
+                    <p>MODERN</p> <p>Sauce, mozzarella, arugula, raw ham, dehydrated tomatoes, parmesan</p>
+                  </div>
                 </div>
               </div>
-            </div>
-            <div className='col-4 images-pizzas-container'>
-              <img className='rounded-4 images-pizza' width="400" height="400" src={veggie} alt="description of image" />
-              <div class="overlay">
-                <div class="text-overlay">
-                  <p>VEGGIE</p> <p>Sauce, muzza, zucchini, roasted, roasted eggplant, candied cherry tomatoes, black olives, and basil leaves</p>
+            
+              
+          
+              <div className='col-4 pizza-image-container'>
+                <img className='rounded-4 zoom-pizza' width="400" height="400" src={onionHam} alt="description of image" />
+                <div class="overlay">
+                  <div class="text-overlay">
+                    <p>ONION HAM</p> <p>Sauce, mozzarella, onion cut into feathers, raw ham, parmesan</p>
+                  </div>
                 </div>
               </div>
-            </div>
-            <div className='col-4 images-pizzas-container'>
-              <img className='rounded-4 images-pizza' width="400" height="400" src={hell} alt="description of image"/>
-              <div class="overlay">
-                <div class="text-overlay">
-                  <p>HELL</p> <p>Sauce, mozza, sliced bacon, sliced green jalapeños, red jalapeño sauce, red onion, shredded provolone and basil leaves</p>
+         
+              <div className='col-4 pizza-image-container'>
+                <img className='rounded-4 zoom-pizza' width="400" height="400" src={german} alt="description of image"/>
+                <div class="overlay">
+                  <div class="text-overlay">
+                    <p>GERMAN</p> <p>Sauce, mozzarella, caramelized onion, grill sausage and mustard</p>
+                  </div>
+                </div>
+              </div>  
+
+            </div> 
+          </div>
+          <div className='mb-3 row '>          
+            <div className='pizzas-container d-flex justify-content-center grid gap-0 column-gap-3'> 
+              <div className='col-4 pizza-image-container'>
+                <img className='rounded-4 zoom-pizza' width="400" height="400" src={onionPepperoni} alt="description of image"/>
+                <div class="overlay">
+                  <div class="text-overlay">
+                    <p>ONION PEPPERONI</p> <p>Sauce, mozzarella, onion cut into feathers, pepperoni</p>
+                  </div>
                 </div>
               </div>
-            </div>  
+            
+              
+          
+              <div className='col-4 pizza-image-container'>
+                <img className='rounded-4 zoom-pizza' width="400" height="400" src={chesses4} alt="description of image" />
+                <div class="overlay">
+                  <div class="text-overlay">
+                    <p>4 CHESSES</p> <p>Sauce, mozzarella, parmesan, fontina, gorgonzola, black olives</p>
+                  </div>
+                </div>
+              </div>
+         
+              <div className='col-4 pizza-image-container'>
+                <img className='rounded-4 zoom-pizza' width="400" height="400" src={pepperoni} alt="description of image"/>
+                <div class="overlay">
+                  <div class="text-overlay">
+                    <p>PEPPERONI</p> <p>Mozzarella, pepperoni, parmesan</p>
+                  </div>
+                </div>
+              </div>  
+
+            </div> 
           </div>
 
-          <div className='mb-3 row d-flex justify-content-center'>
-            <div className='col-4 images-pizzas-container'>
-              <img className='rounded-4 images-pizza' width="400" height="400" src={modern} alt="description of image"/>
-              <div class="overlay">
-                <div class="text-overlay">
-                  <p>MODERN</p> <p>Sauce, mozzarella, arugula, raw ham, dehydrated tomatoes, parmesan</p>
-                </div>
-              </div>
-            </div>
-            <div className='col-4 images-pizzas-container'>
-              <img className='rounded-4 images-pizza' width="400" height="400" src={onionHam} alt="description of image"/>
-              <div class="overlay">
-                <div class="text-overlay">
-                  <p>ONION HAM</p> <p>Sauce, mozzarella, onion cut into feathers, raw ham, parmesan</p>
-                </div>
-              </div>
-            </div>
-            <div className='col-4 images-pizzas-container'>
-              <img className='rounded-4 images-pizza' width="400" height="400" src={german} alt="description of image"/> 
-              <div class="overlay">
-                <div class="text-overlay">
-                  <p>GERMAN</p> <p>Sauce, mozzarella, caramelized onion, grill sausage and mustard</p>
-                </div>
-              </div>
-            </div>  
-          </div>
 
-          <div className='row d-flex justify-content-center'>
-            <div className='col-4 images-pizzas-container'>
-              <img className='rounded-4 images-pizza' width="400" height="400" src={onionPepperoni} alt="description of image"/>
-              <div class="overlay">
-                <div class="text-overlay">
-                  <p>ONION PEPPERONI</p> <p>Sauce, mozzarella, onion cut into feathers, pepperoni</p>
-                </div>
-              </div>
-            </div>
-            <div className='col-4 images-pizzas-container'>
-              <img className='rounded-4 ' width="400" height="400" src={pepperoni} alt="description of image"/>
-              <div class="overlay">
-                <div class="text-overlay">
-                  <p>PEPPERONI</p> <p>Mozzarella, pepperoni, parmesan</p>
-                </div>
-              </div>
-            </div>
-            <div className='col-4 images-pizzas-container'>
-              <img className='rounded-4' width="400" height="400" src={chesses4} alt="description of image"/>
-              <div class="overlay">
-                <div class="text-overlay">
-                  <p>4 CHESSES</p> <p>Sauce, mozzarella, parmesan, fontina, gorgonzola, black olives</p>
-                </div>
-              </div>
-            </div>  
-          </div>
+
+          
         </div>
 
         
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,21 +3,29 @@ import logonav from "../assets/logonav.png";
 
 const Header = () => {
     return (  
-        <div className=''>
-            <nav class="navbar navbar-light bg-faded">
-                <div className=''>
-                    <img width="200" height="100" src={logonav} alt=""/>
+        <div>
+            <nav class=" navbar navbar-light bg-faded fixed-top bg-body-tertiary">
+                <div>
+                    <img width="190" height="70" src={logonav} alt=""/>
                 </div>
                 <div className=''>
                     <ul className='nav nav-navbar pull-xs-right fs-5 fw-normal'>
+                        <li>
+                            <div className="container-fluid">
+                                <form className="d-flex" role="search">
+                                  <input className="form-control me-2" type="search" placeholder="" aria-label="Search"></input>
+                                    <button className="btn btn-light" type="submit"><i class="bi bi-search"></i></button>
+                                </form>
+                            </div>
+                        </li>
                         <li className="nav-item mx-3">
                             <a className="nav-link text-dark" href="#">Home</a>
                         </li>
                         <li className="nav-item mx-3">
-                            <a className="nav-link text-dark" href="#">About</a>
+                            <a className="nav-link text-dark" href="">About</a>
                         </li>
                         <li className="nav-item mx-3">
-                            <a className="nav-link text-dark" href="#">
+                            <a className="nav-link text-dark" href="">
                                 <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" className="bi bi-cart3" viewBox="0 0 16 16">
                                     <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .49.598l-1 5a.5.5 0 0 1-.465.401l-9.397.472L4.415 11H13a.5.5 0 0 1 0 1H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM3.102 4l.84 4.479 9.144-.459L13.89 4H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
                                 </svg>
